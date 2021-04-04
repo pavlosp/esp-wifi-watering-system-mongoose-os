c96e906eb96b66747870c30790a260ba022e6531
Pavlos Papaefstathiou <pavlosp@gmail.com>
2021-04-04 21:29:08 +0100

    Adapted logic to ESP32 and added better error handling and retry logic

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# ESP8266 WiFi controlled watering can
+# ESP8266 / ESP32 WiFi controlled watering can
 
 ## Overview
 
-First attempt at automating watering of plants. 
+First attempt at automating watering of plants.
 
 Measures soil humidity, as well as air temperature and relative humidity.
 Based on soilhumidity reading, it controls a motor (via N-channel MOSFET)
 to water the plant for a specified period of time. Device can then sleep
-for a specified period of time (up to approx. 70 mins for ESP8266).
+for a specified period of time (up to approx. 70 mins for ESP8266; longer
+for ESP32).
 
 Config and state is sync'ed with GCP IOT Registry, and readings are sent
-via MQTT to GCP Pub/Sub (and subsequently stored on Firebase database for
+via MQTT to GCP Pub/Sub (and subsequently stored on Firestore for
 analysis).
-
--- fs/init.js
+++ fs/init.js
@@ -5,96 +5,111 @@ load('api_mqtt.js');
 load('api_sys.js');
 load('api_timer.js');
 load('api_gpio.js');
-load('api_esp8266.js');
+load('api_esp32.js');
 load('api_math.js');
 load('api_dht.js');
 
-let led_pin = 15;
+let led_pin = 4;
 GPIO.setup_output(led_pin, 0);
 
-let adc_sensor_pin = 0;
-let adc_enable = ADC.enable(0);
+let adc_sensor_pin = 33;
+let adc_enable = ADC.enable(adc_sensor_pin);
 print('INIT: Enabled ADC pin ', adc_sensor_pin, ' with success: ', adc_enable);
 
-let motor_gpio_pin = 13;
+let motor_gpio_pin = 27;
 GPIO.setup_output(motor_gpio_pin, 0);
 
-let dht_gpio_pin = 4;
+let dht_gpio_pin = 18;
 let dht = DHT.create(dht_gpio_pin, DHT.DHT11);
 
-let MAX_USEC_TO_SLEEP = 4294967295;
-let MIN_IN_USEC = 1 * 60 * 1000000;
+let MIN_IN_USEC = 1 * 60 * 1000000; // 1 min in usecs
+let SEC_IN_USEC = 1000000;
+
+let peripherals_switch_pin = 21;
+GPIO.setup_output(peripherals_switch_pin, 0);
 
 let state = {
   mustSleep: 0,
-  ledState: 0, 
-  minutesToSleep: 70,
-  humidityThreshold: 45,
-  wateringDurationMsec: 2500
+  ledState: 0,
+  minutesToSleep: 240,
+  humidityThreshold: 50,
+  wateringDurationMsec: 2500,
+  busy: 0,
+};
+
+let response = {
+  success: 1,
 };
 
 function readADCSensor() {
   let adc_reading = ADC.read(adc_sensor_pin);
-  let adc_percentage = adc_reading * 100 / 1024;
+  let adc_percentage = adc_reading * 100.0 / 4095.0;
   return {
     adc_reading: adc_reading,
-    adc_percentage: adc_percentage
+    adc_percentage: adc_percentage * 1.0,
   };
 }
 
 function sleep() {
   print('Entering deep sleep...');
-  let usec_to_sleep = Math.max(MAX_USEC_TO_SLEEP, state.minutesToSleep * MIN_IN_USEC);
-  ESP8266.deepSleep(usec_to_sleep); // sleep for specified usecs (up to max)
-  // when restarts, it will re-initialise itself
+  let usec_to_sleep = state.minutesToSleep * MIN_IN_USEC;
+  ESP32.deepSleep(usec_to_sleep); // sleep for specified usecs (up to max)
+  // when ESP restarts, it will re-initialise itself
 }
 
 function waterPlant() {
-    GPIO.write(motor_gpio_pin, 1);
-    Sys.usleep(state.wateringDurationMsec*1000);
+  GPIO.write(motor_gpio_pin, 1);
+
+  // wait for the required duration and stop motor
+  Timer.set(state.wateringDurationMsec, false, function () {
     GPIO.write(motor_gpio_pin, 0);
+  }, null);
 }
 
-RPC.addHandler('ADC.Read', function(args) {
+RPC.addHandler('ADC.Read', function (args) {
   let r = readADCSensor();
 
   return {
     adc_reading: r.adc_reading,
-    adc_percentage: r.adc_percentage,
+    adc_percentage: r.adc_percentage * 1.0,
     success: 1
   };
 });
 
-RPC.addHandler('ToggleLED.Action', function(args) {
+RPC.addHandler('ToggleLED.Action', function (args) {
   state.ledState = !state.ledState;
   GPIO.write(led_pin, state.ledState);
-  return { success: 1 };
+  return {
+    success: 1
+  };
 });
 
-RPC.addHandler('Board.Sleep', function(args) {
+RPC.addHandler('Board.Sleep', function (args) {
   sleep();
 });
 
-RPC.addHandler('WaterPlant.Action', function(args) {
+RPC.addHandler('WaterPlant.Action', function (args) {
   waterPlant();
-  return { success: 1 };
+  return {
+    success: 1
+  };
 });
 
-RPC.addHandler('TempHumidity.Read', function(args) {
+RPC.addHandler('TempHumidity.Read', function (args) {
   let t = dht.getTemp();
   let h = dht.getHumidity();
 
   if (isNaN(h) || isNaN(t)) {
     return {
       success: 0,
-      error: 'ERROR: Failed to get temp and humidity readings'
+      error: 'DHT_SENSOR_FAIL: Failed to get temp and humidity readings'
     };
   }
 
   // else return the temp and humidity in an object
   return {
     temp: t,
-    humidity: h, 
+    humidity: h,
     success: 1
   };
 });
@@ -103,54 +118,122 @@ let stateTopic = '/devices/' + Cfg.get('device.id') + '/state';
 let iotTopic = '/devices/' + Cfg.get('device.id') + '/events';
 let gcp_config_topic = '/devices/' + Cfg.get('device.id') + '/config';
 
-function sendState() { 
+function sendState() {
   // send status update to GCP
-  let msg = JSON.stringify({free: Sys.free_ram(), total: Sys.total_ram(), adc_enable: adc_enable});
+  let msg = JSON.stringify({
+    free: Sys.free_ram(),
+    total: Sys.total_ram(),
+    adc_enable: adc_enable
+  });
   print(stateTopic, '->', msg);
   let ok = MQTT.pub(stateTopic, msg, 1);
   print('GCP STATUS message published ok: ', ok);
 }
 
-function sendMeterReadingsAndWater() {
+function checkWateringResult() {
+  print('Checking result of watering...');
   let adc_r = readADCSensor();
-  let soil_humidity = 100 - adc_r.adc_percentage; // soil humidity reading is 0 when sensor immersed in water
+  let soil_humidity = Math.round((100 - adc_r.adc_percentage) * 10.0) / 10.0; // get new reading
 
-  let response = { 
-    success: 1, 
-    soil_humidity: Math.floor(soil_humidity),
+  print('New soil humidity: ', soil_humidity, ' vs old soil humidity: ', response.soil_humidity);
+  // if there's no material movement in the reading, something is wrong
+  if (soil_humidity < response.soil_humidity + 1.0) {
+    print('Something went wrong with the watering; increasing duration');
+
+    // try increasing the watering duration
+    state.wateringDurationMsec = state.wateringDurationMsec * 1.5;
+
+    // set an error message
+    response.success = 0;
+    response.error = 'PUMP_OR_HUMIDITY_SENSOR_FAIL: Watering pump or soil humidity sensor failure';
+  }
+
+  if ((soil_humidity < state.humidityThreshold) && response.attempt < 5) {
+    response.attempt++;
+    response.watered_flag = 1; // set the 'watered_flag' field to true (1) 
+    
+    print('Watering plant');
+    waterPlant();
+    
+    // wait for a few seconds and check soil humidity to ensure that watering has taken place
+    Timer.set(state.wateringDurationMsec + 30 * 1000, false, checkWateringResult, null);
+  } else {
+    response.soil_humidity = soil_humidity;
+    let r = JSON.stringify(response); 
+
+    // Send sensor readings and watered_flag to GCP IOT Cloud
+    print('Sensor readings to submit to GCP Cloud: ', r);
+    let ok = MQTT.pub(iotTopic, r, 1);
+    print('Message published ok: ', ok);
+
+    Timer.set(10 * 1000, false, function () { state.busy = 0 }, null);
+  }
+}
+
+function sendMeterReadingsAndWater() {
+  state.busy = 1;
+
+  let adc_r = readADCSensor();
+  adc_r = readADCSensor();
+  adc_r = readADCSensor();
+
+  // reverse the reading; soil humidity reading is 0 when sensor immersed in water
+  let soil_humidity = Math.round((100 - adc_r.adc_percentage) * 10.0) / 10.0;
+
+  response = {
+    success: 1,
+    soil_humidity: soil_humidity * 1.0,
   };
 
   let t = dht.getTemp();
   let h = dht.getHumidity();
-  
+
   if (!isNaN(h)) {
     response.humidity = h;
   }
-  
+
   if (!isNaN(t)) {
     response.temp = t;
-  }  
-
-  // If soil_humidity is less than WATERED_THRESHOLD%, then water the plant automatically for 1.5 sec
-  if (response.soil_humidity < state.humidityThreshold) {
-    waterPlant();
-
-    // set the 'watered_flag' field to true (1) in the response to know that we have watered the plant
-    response.watered_flag = 1;
   }
 
-  let r = JSON.stringify(response);
+  // if soil humidity is super high (e.g. higher than 90%), something is wrong, report
+  if (soil_humidity >= 90.0) {
+    response.success = 0;
+    response.error = 'SOIL_HUMIDITY_SENSOR_FAIL: Soil humidity sensor failure; cannot be higher than 90%';
+    print('Soil humidity sensor failure; cannot be higher than 90%');
+  }
 
-  // Send sensor readings and watered_flag to GCP IOT Cloud
-  print('Sensor readings to submit to GCP Cloud: ', r);
-  let ok = MQTT.pub(iotTopic, r, 1);
-  print('Message published ok: ', ok);
+  if (soil_humidity < state.humidityThreshold) {
+    response.attempt = 1;
+    response.watered_flag = 1; // set the 'watered_flag' field to true (1) 
+    
+    print('Watering plant');
+    waterPlant();
+    
+    // wait for a few seconds and check soil humidity to ensure that watering has taken place
+    Timer.set(state.wateringDurationMsec + 30 * 1000, false, checkWateringResult, null);
+  } else {
+    let r = JSON.stringify(response);
+
+    // Send sensor readings and watered_flag to GCP IOT Cloud
+    print('Sensor readings to submit to GCP Cloud: ', r);
+    let ok = MQTT.pub(iotTopic, r, 1);
+    print('Message published ok: ', ok);
+
+    Timer.set(10 * 1000, false, function () { state.busy = 0 }, null);
+  }
 }
 
 // subscribe to config changes (update state)
-MQTT.sub(gcp_config_topic, function(conn, t, msg) {
+MQTT.sub(gcp_config_topic, function (conn, t, msg) {
   print('Topic:', t, 'message:', msg);
-  let obj = JSON.parse(msg) || { led_state: 1, must_sleep: 0, humidity_threshold: 45, minutes_to_sleep: 70, watering_duration_msec: 2000};
+  let obj = JSON.parse(msg) || {
+    led_state: 1,
+    must_sleep: 0,
+    humidity_threshold: 50,
+    minutes_to_sleep: 240,
+    watering_duration_msec: 2500
+  };
 
   state.ledState = obj.led_state;
   state.mustSleep = obj.must_sleep;
@@ -161,39 +244,41 @@ MQTT.sub(gcp_config_topic, function(conn, t, msg) {
   GPIO.write(led_pin, state.ledState);
 }, null);
 
-// if connection has been established with GCP, send data, water if needed, then sleep for ~15 mins
-MQTT.setEventHandler(function(conn, ev, edata) {
-	if (ev === MQTT.EV_CONNACK) {
+// if connection has been established with GCP, send data, water if needed, then sleep
+MQTT.setEventHandler(function (conn, ev, edata) {
+  if (ev === MQTT.EV_CONNACK) {
     print("MQTT connection established.");
 
     sendState();
 
-    // wait for 5 sec to ensure accurate sensor readings and update config state
-    Timer.set(1000 * 5, false, function() {
-      
+    // wait for 10 sec to ensure accurate sensor readings and update config state
+    Timer.set(1000 * 10, false, function () {
+
       sendMeterReadingsAndWater();
 
       if (state.mustSleep) { // check if we are required to sleep to save energy
-        // Wait a moment to ensure the message has really been sent 
-        Timer.set(1000 * 2, false, sleep, null);
+        // Wait a few seconds to ensure state and data has been sent
+        // Timer repeats as it will be checking to ensure 'busy' state is set to 0 before deep sleep
+        Timer.set(1000 * 10, Timer.REPEAT, function () { if (!state.busy) { sleep(); } }, null);
       }
 
     }, null);
-	}
+  }
 }, null);
 
 // set a timer to repeat the watering sequence. This is useful in case we have paused "sleeping" (config change)
-Timer.set(1000 * 60 * state.minutesToSleep /* repeat */, Timer.REPEAT, function() {
+Timer.set(1000 * 60 * state.minutesToSleep /* repeat */ , Timer.REPEAT, function () {
   sendState();
 
-  // wait for 5 sec to ensure accurate sensor readings and update config state
-  Timer.set(1000 * 5, false, function() {
-    
+  // wait for 10 sec to ensure accurate sensor readings and update config state
+  Timer.set(1000 * 10, false, function () {
+
     sendMeterReadingsAndWater();
 
     if (state.mustSleep) { // check if we are required to sleep to save energy
-      // Wait a moment to ensure the message has really been sent 
-      Timer.set(1000 * 2, false, sleep, null);
+      // Wait a few seconds to ensure state and data has been sent
+      // Timer repeats as it will be checking to ensure 'busy' state is set to 0 before deep sleep
+      Timer.set(1000 * 10, Timer.REPEAT, function () { if (!state.busy) { sleep(); } }, null);
     }
 
   }, null);
